3cbf67005a3735b806bba8d80dc2fe11ebd45d72
Renato Uchoa <contato@renatouchoa.com.br>
2016-10-13 14:41:38 -0300

    Acréscimo do diretório de publicação do processo de desenvolvimento

---
+++ processo/scripts/processElementData.js
@@ -0,0 +1,106 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_PONbIH9rEeaF1pjERfVvAA,_TNBaEX9rEeaF1pjERfVvAA,_F159EX9tEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/iteracao_gerenciar_329CAACF_cbef12a6_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_PONbIH9rEeaF1pjERfVvAA,_TNBaEX9rEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/Iteração única I1_2D610397_860c7283_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_H--0UX9rEeakyew3j8_LfQ,_Mf1B8X9uEeaF1pjERfVvAA,_PXQiEX9uEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/iteracao_planejar_gerenciar_9EA48C81_eec44957_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_QgBBQX9rEeaF1pjERfVvAA,_bjhK8X9rEeaF1pjERfVvAA,_eMLJ0YAcEeasuNwaWkn8fgActivity"]="./../../Enutri/deliveryprocesses/resources/uc_implementar_12284284_cd906545_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_QgBBQX9rEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/Construção_23F49068_15d893c3_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_PONbIH9rEeaF1pjERfVvAA,_TNBaEX9rEeaF1pjERfVvAA,_eLYy0X9wEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/projeto_iniciar_BBE5707F_5537d856_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_H--0UX9rEeakyew3j8_LfQActivity"]="./../../Enutri/deliveryprocesses/resources/Elaboração_BA1306FB_abf70a56_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_QgBBQX9rEeaF1pjERfVvAA,_bjhK8X9rEeaF1pjERfVvAA,_P6H-kX9uEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/iteracao_planejar_gerenciar_C75C0F66_82c43227_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_H--0UX9rEeakyew3j8_LfQ,_qLncwX9rEeaF1pjERfVvAA,_rl2UMX9tEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/iteracao_planejar_gerenciar_252591D5_4c6b074f_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_H--0UX9rEeakyew3j8_LfQ,_VMILEX9rEeaF1pjERfVvAA,_qfg-QX9tEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/iteracao_planejar_gerenciar_3733BA2F_32ad7445_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_PONbIH9rEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/Concepção_D0CD253D_c2b12898_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_H--0UX9rEeakyew3j8_LfQ,_odo4AX9rEeaF1pjERfVvAA,_q_QOQX9tEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/iteracao_planejar_gerenciar_47BD9320_97b608da_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_QgBBQX9rEeaF1pjERfVvAA,_bjhK8X9rEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/Iteração de construção ...n_A3650256_6e83cb6d_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_H--0UX9rEeakyew3j8_LfQ,_h1Z_wX9rEeaF1pjERfVvAA,_skSSYX9tEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/iteracao_planejar_gerenciar_1670CF24_9a0fcffc_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_H--0UX9rEeakyew3j8_LfQ,_odo4AX9rEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/Projeto do banco de dados E_F0D8C2BC_83503966_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_H--0UX9rEeakyew3j8_LfQ,_VMILEX9rEeaF1pjERfVvAA,_g1xMIY_cEea4GYIP2O4DgwActivity"]="./../../Enutri/deliveryprocesses/resources/requisitos_identificar_refinar_FF8E539B_fb080db1_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_PONbIH9rEeaF1pjERfVvAA,_TNBaEX9rEeaF1pjERfVvAA,_k2orIYAmEeasuNwaWkn8fgActivity"]="./../../Enutri/deliveryprocesses/resources/requisitos_identificar_67B98E59_10bf630_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQActivity"]="./../../Enutri/deliveryprocesses/resources/Ciclo de Vida_4A3B5A07_4a3b5a07_Activity.jpeg"
+contentPage.processPage.imageFiles["_xLjf0X9qEeakyew3j8_LfQ,_H--0UX9rEeakyew3j8_LfQ,_VMILEX9rEeaF1pjERfVvAAActivity"]="./../../Enutri/deliveryprocesses/resources/Elaboração_A4465B18_36bdd1c2_Activity.jpeg"
+contentPage.processPage.elementUrls["_zZBisIv8Eea9K-5vLV5Vkw_qlUKoH91EeaF1pjERfVvAA"] = ["Stakeholder","Enutri/capabilitypatterns/stakeholder_2B644B04.html"];
+contentPage.processPage.elementUrls["_zZBisIv8Eea9K-5vLV5Vkw_tJIyQH91EeaF1pjERfVvAA"] = ["Desenvolvedor","Enutri/capabilitypatterns/desenvolvedor_2293E457.html"];
+contentPage.processPage.elementUrls["_zZBisIv8Eea9K-5vLV5Vkw_lNOREIAkEeasuNwaWkn8fg"] = ["Glossário","Enutri/capabilitypatterns/glossario_32CD7F56.html"];
+contentPage.processPage.elementUrls["_08VuQI_bEea4GYIP2O4Dgw"] = ["Detalhar os Requisitos","Enutri/capabilitypatterns/requisitos_detalhar_9038862A.html"];
+contentPage.processPage.elementUrls["_b_2A8H9zEeaF1pjERfVvAA_tJIyQH91EeaF1pjERfVvAA"] = ["Desenvolvedor","Enutri/capabilitypatterns/desenvolvedor_5E07E3F8.html"];
+contentPage.processPage.elementUrls["_b__K4H9zEeaF1pjERfVvAA_8h4CYH9rEeaF1pjERfVvAA"] = ["Visão","Enutri/capabilitypatterns/visao_479EA1E9.html"];
+contentPage.processPage.elementUrls["_Fu0SEH93EeaF1pjERfVvAA"] = ["Marco da Arquitetura","Enutri/deliveryprocesses/Marco da Arquitetura_D0EC441.html"];
+contentPage.processPage.elementUrls["_HW1OMH92EeaF1pjERfVvAA"] = ["Modelar o Banco de Dados","Enutri/capabilitypatterns/db_modelar_1BD96A73.html"];
+contentPage.processPage.elementUrls["_HYeM8n92EeaF1pjERfVvAA"] = ["Modelo de entidades e relacionamentos","Enutri/capabilitypatterns/db_er_7FD584DA.html"];
+contentPage.processPage.elementUrls["_Mf1B8X9uEeaF1pjERfVvAA"] = ["Planejamento da construção [Ed+1..Ee]","Enutri/deliveryprocesses/Planejamento da construção_76D89DD8.html"];
+contentPage.processPage.elementUrls["_eMLJ0YAcEeasuNwaWkn8fg"] = ["Implementar Caso de Uso","Enutri/deliveryprocesses/uc_implementar_12284284.html"];
+contentPage.processPage.elementUrls["_RS0-QX9rEeaF1pjERfVvAA"] = ["Transição","Enutri/deliveryprocesses/Transição_862C0570.html"];
+contentPage.processPage.elementUrls["_xLjf0X9qEeakyew3j8_LfQ"] = ["Ciclo de Vida","Enutri/deliveryprocesses/Ciclo de Vida_4A3B5A07.html"];
+contentPage.processPage.elementUrls["_VMILEX9rEeaF1pjERfVvAA"] = ["Análise de Requisitos [E1..Ea]","Enutri/deliveryprocesses/Elaboração_A4465B18.html"];
+contentPage.processPage.elementUrls["_ZGhAMX9vEeaF1pjERfVvAA"] = ["Implementar o Banco de Dados","Enutri/deliveryprocesses/db_implementar_350B00C0.html"];
+contentPage.processPage.elementUrls["_HYeM8H92EeaF1pjERfVvAA"] = ["Stakeholder","Enutri/capabilitypatterns/stakeholder_869E90B4.html"];
+contentPage.processPage.elementUrls["_GT4UcIAbEeasuNwaWkn8fg"] = ["Implementar Caso de Uso","Enutri/capabilitypatterns/uc_implementar_8A67F6A8.html"];
+contentPage.processPage.elementUrls["_H--0UX9rEeakyew3j8_LfQ"] = ["Elaboração","Enutri/deliveryprocesses/Elaboração_BA1306FB.html"];
+contentPage.processPage.elementUrls["_F159EX9tEeaF1pjERfVvAA"] = ["Planejar e gerenciar a iteração","Enutri/deliveryprocesses/iteracao_gerenciar_329CAACF.html"];
+contentPage.processPage.elementUrls["_HYVDAH92EeaF1pjERfVvAA"] = ["Desenvolvedor","Enutri/capabilitypatterns/desenvolvedor_A2468471.html"];
+contentPage.processPage.elementUrls["_b_2A8H9zEeaF1pjERfVvAA_qlUKoH91EeaF1pjERfVvAA"] = ["Stakeholder","Enutri/capabilitypatterns/stakeholder_66D84AA5.html"];
+contentPage.processPage.elementUrls["_FfuToIAbEeasuNwaWkn8fg"] = ["Testar Implementação","Enutri/capabilitypatterns/uc_testar_1FE724ED.html"];
+contentPage.processPage.elementUrls["_3pyJEYwDEea9K-5vLV5Vkw"] = ["Manter o Backlog","Enutri/deliveryprocesses/backlog_manter_9DAFC320.html"];
+contentPage.processPage.elementUrls["_TNBaEX9rEeaF1pjERfVvAA"] = ["Iteração de concepção [I1..In]","Enutri/deliveryprocesses/Iteração única I1_2D610397.html"];
+contentPage.processPage.elementUrls["_K0CnMIvzEeaojuFrSEQLzQ_lNOREIAkEeasuNwaWkn8fg"] = ["Glossário","Enutri/capabilitypatterns/glossario_92B1548E.html"];
+contentPage.processPage.elementUrls["_rl2UMX9tEeaF1pjERfVvAA"] = ["Planejar e gerenciar a iteração","Enutri/deliveryprocesses/iteracao_planejar_gerenciar_252591D5.html"];
+contentPage.processPage.elementUrls["_K0CnMIvzEeaojuFrSEQLzQ"] = ["Planejar o Projeto","Enutri/capabilitypatterns/projeto_planejar_1E41C678.html"];
+contentPage.processPage.elementUrls["_qLncwX9rEeaF1pjERfVvAA"] = ["Arquitetura [Ea+1..Eb]","Enutri/deliveryprocesses/Projeto da arquitetura E_20F9027C.html"];
+contentPage.processPage.elementUrls["_KDnP0IvzEeaojuFrSEQLzQ"] = ["Definir a Visão","Enutri/capabilitypatterns/visao_definir_A3E5E460.html"];
+contentPage.processPage.elementUrls["_b_2A8H9zEeaF1pjERfVvAA_0fZYEIwAEea9K-5vLV5Vkw"] = ["Plano de Iteração","Enutri/capabilitypatterns/iteracao_plano_DC23F25.html"];
+contentPage.processPage.elementUrls["_cgajEZFpEeanrp-HoE7FZQ"] = ["Backlog","Enutri/capabilitypatterns/backlog_4BCBDF0D.html"];
+contentPage.processPage.elementUrls["_h1Z_wX9rEeaF1pjERfVvAA"] = ["Interface com o usuário [Ec+1..Ed]","Enutri/deliveryprocesses/Projeto da Interface com o usuário E_C2D547DA.html"];
+contentPage.processPage.elementUrls["_b_2A8H9zEeaF1pjERfVvAA"] = ["Avaliar a iteração","Enutri/capabilitypatterns/iteracao_avaliar_F844DE2F.html"];
+contentPage.processPage.elementUrls["_KDnP0IvzEeaojuFrSEQLzQ_tJIyQH91EeaF1pjERfVvAA"] = ["Desenvolvedor","Enutri/capabilitypatterns/desenvolvedor_72EC76A7.html"];
+contentPage.processPage.elementUrls["_OgCwwH9vEeaF1pjERfVvAA"] = ["Implementar o Banco de Dados","Enutri/capabilitypatterns/db_implementar_1D48880C.html"];
+contentPage.processPage.elementUrls["_eLYy0X9wEeaF1pjERfVvAA"] = ["Iniciar o Projeto","Enutri/deliveryprocesses/projeto_iniciar_BBE5707F.html"];
+contentPage.processPage.elementUrls["_I_PugIAbEeasuNwaWkn8fg_tJIyQH91EeaF1pjERfVvAA"] = ["Desenvolvedor","Enutri/capabilitypatterns/desenvolvedor_1E5FCC76.html"];
+contentPage.processPage.elementUrls["_8bLfsYv8Eea9K-5vLV5Vkw"] = ["Visão","Enutri/capabilitypatterns/visao_8F7AC63D.html"];
+contentPage.processPage.elementUrls["_qfg-QX9tEeaF1pjERfVvAA"] = ["Planejar e gerenciar a iteração","Enutri/deliveryprocesses/iteracao_planejar_gerenciar_3733BA2F.html"];
+contentPage.processPage.elementUrls["_eMMHQX90EeaF1pjERfVvAA"] = ["Projetar o Banco de Dados","Enutri/deliveryprocesses/db_projetar_FA3F5193.html"];
+contentPage.processPage.elementUrls["_b__K4H9zEeaF1pjERfVvAA_LWnMkH9tEeaF1pjERfVvAA"] = ["Cronograma","Enutri/capabilitypatterns/cronograma_FD340684.html"];
+contentPage.processPage.elementUrls["_LrMV0YvsEeaxV7u7ViJ4Og"] = ["Treinamento [Ta+1..Eb]","Enutri/deliveryprocesses/Treinamento T2_1FBFCC03.html"];
+contentPage.processPage.elementUrls["_XDEaYH9yEeaF1pjERfVvAA"] = ["Marco da capacidade operacional inicial","Enutri/deliveryprocesses/Marco da capacidade operacional inicial_462B8C49.html"];
+contentPage.processPage.elementUrls["_ZJU8QH9yEeaF1pjERfVvAA"] = ["Marco da liberação do produto","Enutri/deliveryprocesses/Marco da liberação do produto_4116F9C.html"];
+contentPage.processPage.elementUrls["_zZBisIv8Eea9K-5vLV5Vkw"] = ["Encontrar e descrever os Requisitos","Enutri/capabilitypatterns/requisitos_encontrar_descrever_2B3C1CB0.html"];
+contentPage.processPage.elementUrls["_odo4AX9rEeaF1pjERfVvAA"] = ["Banco de dados [Eb+1..Ec]","Enutri/deliveryprocesses/Projeto do banco de dados E_F0D8C2BC.html"];
+contentPage.processPage.elementUrls["_I_PugIAbEeasuNwaWkn8fg_JNd7QIAcEeasuNwaWkn8fg"] = ["Repositório de Testes","Enutri/capabilitypatterns/branch_teste_8202C33.html"];
+contentPage.processPage.elementUrls["_OgCwwH9vEeaF1pjERfVvAA_A-ILQH9sEeaF1pjERfVvAA"] = ["Banco de Dados de Testes","Enutri/capabilitypatterns/db_testes_A3F4AA06.html"];
+contentPage.processPage.elementUrls["_b__K4H9zEeaF1pjERfVvAA"] = ["Planejar a iteração","Enutri/capabilitypatterns/iteracao_planejar_A617D5F0.html"];
+contentPage.processPage.elementUrls["_q_QOQX9tEeaF1pjERfVvAA"] = ["Planejar e gerenciar a iteração","Enutri/deliveryprocesses/iteracao_planejar_gerenciar_47BD9320.html"];
+contentPage.processPage.elementUrls["_KDnP0IvzEeaojuFrSEQLzQ_46j9cH9rEeaF1pjERfVvAA"] = ["Plano do Projeto","Enutri/capabilitypatterns/projeto_plano_E9C6B381.html"];
+contentPage.processPage.elementUrls["_FfuToIAbEeasuNwaWkn8fg_WpIPIIAcEeasuNwaWkn8fg"] = ["Script de Teste","Enutri/capabilitypatterns/teste_script_243D3B48.html"];
+contentPage.processPage.elementUrls["_bjhK8X9rEeaF1pjERfVvAA"] = ["Iteração de construção [C1..Cn]","Enutri/deliveryprocesses/Iteração de construção ...n_A3650256.html"];
+contentPage.processPage.elementUrls["_0nD-8IwDEea9K-5vLV5Vkw"] = ["Manter o Backlog","Enutri/capabilitypatterns/backlog_manter_AA17D816.html"];
+contentPage.processPage.elementUrls["_PONbIH9rEeaF1pjERfVvAA"] = ["Concepção","Enutri/deliveryprocesses/Concepção_D0CD253D.html"];
+contentPage.processPage.elementUrls["_P6H-kX9uEeaF1pjERfVvAA"] = ["Planejar e gerenciar a iteração","Enutri/deliveryprocesses/iteracao_planejar_gerenciar_C75C0F66.html"];
+contentPage.processPage.elementUrls["_fX4eUX90EeaF1pjERfVvAA"] = ["Modelar o Banco de Dados","Enutri/deliveryprocesses/db_modelar_CB1E44CF.html"];
+contentPage.processPage.elementUrls["_PXQiEX9uEeaF1pjERfVvAA"] = ["Planejar e gerenciar a iteração","Enutri/deliveryprocesses/iteracao_planejar_gerenciar_9EA48C81.html"];
+contentPage.processPage.elementUrls["_I_PugIAbEeasuNwaWkn8fg_GcmmIH9sEeaF1pjERfVvAA"] = ["Repositório de Produção","Enutri/capabilitypatterns/branch_master_A895DC36.html"];
+contentPage.processPage.elementUrls["_SyYBQH9yEeaF1pjERfVvAA"] = ["Marcos dos Objetivos","Enutri/deliveryprocesses/Marcos dos objetivos_4DAD92F8.html"];
+contentPage.processPage.elementUrls["_k2orIYAmEeasuNwaWkn8fg"] = ["Identificar e Refinar os Requisitos","Enutri/deliveryprocesses/requisitos_identificar_67B98E59.html"];
+contentPage.processPage.elementUrls["_skSSYX9tEeaF1pjERfVvAA"] = ["Planejar e gerenciar a iteração","Enutri/deliveryprocesses/iteracao_planejar_gerenciar_1670CF24.html"];
+contentPage.processPage.elementUrls["_FH4bYIAbEeasuNwaWkn8fg"] = ["Modelar Teste de Caso de Uso","Enutri/capabilitypatterns/uc_teste_modelar_30AADB94.html"];
+contentPage.processPage.elementUrls["_OgCwwH9vEeaF1pjERfVvAA_OaEzAH9sEeaF1pjERfVvAA"] = ["Modelo lógico do banco de dados","Enutri/capabilitypatterns/db_logico_9B4ED22E.html"];
+contentPage.processPage.elementUrls["_KDnP0IvzEeaojuFrSEQLzQ_8h4CYH9rEeaF1pjERfVvAA"] = ["Visão","Enutri/capabilitypatterns/visao_F88F5579.html"];
+contentPage.processPage.elementUrls["_g1xMIY_cEea4GYIP2O4Dgw"] = ["Identificar e Refinar os Requisitos","Enutri/deliveryprocesses/requisitos_identificar_refinar_FF8E539B.html"];
+contentPage.processPage.elementUrls["_KDnP0IvzEeaojuFrSEQLzQ_qlUKoH91EeaF1pjERfVvAA"] = ["Stakeholder","Enutri/capabilitypatterns/stakeholder_7BBCDD54.html"];
+contentPage.processPage.elementUrls["_I_PugIAbEeasuNwaWkn8fg"] = ["Integrar Implementação","Enutri/capabilitypatterns/uc_integrar_3F4D2B71.html"];
+contentPage.processPage.elementUrls["_zZBisIv8Eea9K-5vLV5Vkw_NbRz8IAmEeasuNwaWkn8fg"] = ["Modelo de Casos de Uso","Enutri/capabilitypatterns/uc_modelo_FCE7F0FC.html"];
+contentPage.processPage.elementUrls["_b_2A8H9zEeaF1pjERfVvAA_46j9cH9rEeaF1pjERfVvAA"] = ["Plano do Projeto","Enutri/capabilitypatterns/projeto_plano_D4E220D2.html"];
+contentPage.processPage.elementUrls["_bGBdAH9zEeaF1pjERfVvAA"] = ["Gerenciar o tempo do projeto","Enutri/capabilitypatterns/tempo_gerenciar_4B779081.html"];
+contentPage.processPage.elementUrls["_GWjzcYvsEeaxV7u7ViJ4Og"] = ["Implantação do sistema [T1..Ea]","Enutri/deliveryprocesses/Implantar o sistema T1_7D5C5661.html"];
+contentPage.processPage.elementUrls["_zZBisIv8Eea9K-5vLV5Vkw_46j9cH9rEeaF1pjERfVvAA"] = ["Plano do Projeto","Enutri/capabilitypatterns/projeto_plano_996E2131.html"];
+contentPage.processPage.elementUrls["_QgBBQX9rEeaF1pjERfVvAA"] = ["Construção","Enutri/deliveryprocesses/Construção_23F49068.html"];
