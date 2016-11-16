4b4ba00581ddde76815157d2fc1819099150cef5
Renato Uchoa <renatoubra@gmail.com>
2016-11-16 16:38:23 -0300

    Refatoração da classe ButtonHelper
    - Melhorias no método make()
    
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

--- src/View/Helper/ButtonHelper.php
+++ src/View/Helper/ButtonHelper.php
@@ -7,19 +7,6 @@ use Cake\View\Helper;
 /**
  * Constroi botões
  * 
- * Métodos que podem ser invocados dinamicamente
- * @method string default(string $text, array $options)
- * @method string primary(string $text, array $options)
- * @method string success(string $text, array $options)
- * @method string info(string $text, array $options)
- * @method string danger(string $text, array $options)
- * @method string warning(string $text, array $options)
- * @method string maroom(string $text, array $options)
- * @method string purble(string $text, array $options)
- * @method string navy(string $text, array $options)
- * @method string orange(string $text, array $options)
- * @method string olive(string $text, array $options)
- * 
  * @return string HTML do botão
  * 
  * @author Renato Uchoa <contato@renatouchoa.com.br>
@@ -34,49 +21,13 @@ class ButtonHelper extends Helper
     public $helpers = ['Html', 'Icon'];
     
     /**
-     * Classes CSS para tamanhos pré-definidos
-     * 
-     * @var array
-     */
-    protected $sizes = [
-        'normal' => '',
-        'large'  => 'btn-lg',
-        'small'  => 'btn-sm',
-        'xsmall' => 'btn-xs',
-    ];
-    
-    /**
-     * Classes CSS para estilos pré-definidos
-     * 
-     * @var array
-     */
-    protected $styles = [
-        'default' => 'btn-default',
-        'primary' => 'btn-primary',
-        'success' => 'btn-success',
-        'info'    => 'btn-info',
-        'danger'  => 'btn-danger',
-        'warning' => 'btn-warning',
-        'maroom'  => 'bg-maroom',
-        'purple'  => 'bg-purple',
-        'navy'    => 'bg-navy',
-        'orange'  => 'bg-orange',
-        'olive'   => 'bg-olive',
-        'link'    => 'btn-link',
-    ];
-    
-    /**
      * Constroi o botão
      * 
      * @param array $options
      * As chaves não reservadas serão inseridas como atributo/valor da tag
      * Chaves reservadas:
      * 'text'     Texto apresentado no botão
-     * 'size'     Tamanos pré definidos (normal, large, small, xsmall)
-     * 'style'    Estilos pré definidos (default, primary, succcess, etc...)
      * 'url       Href do botão  
-     * 'flat'     Boolean
-     * 'block'    Boolean
      * 'icon'     Classe do ícone (ou alias). Veja o IconHelper
      * 'iconLeft' Se falso, o ícone será construído à direita do texto
      * 'tag'      Tipo da tag do botão (link, button)
@@ -92,12 +43,8 @@ class ButtonHelper extends Helper
          */
         $defaultOptions = [
             'text'     => '',
-            'class'    => '',
-            'size'     => 'normal',
-            'style'    => 'default',
+            'class'    => 'btn-default',
             'url'      => '#',
-            'flat'     => false,
-            'block'    => false,
             'icon'     => null,
             'iconLeft' => true,
             'tag'      => 'link',
@@ -106,61 +53,28 @@ class ButtonHelper extends Helper
         
         $options = array_merge($defaultOptions, $options);
         
-        /**
-         * Classes CSS básicas para formatar o botão
-         * 
-         * @var string
-         */
-        $classes = ['btn'];
+        $options['class'] = trim('btn ' . $options['class']);
         
         // Configura o texto apresentado no botão
-        $text = $options['text'];
+        $textHtml = $options['text'];
         unset($options['text']);
-        
-        // Configura o tamanho do botão...
-        if (!array_key_exists($options['size'], $this->sizes)) {
-            $options['size'] = 'normal';
-        }
-        $classes[] = $this->sizes[$options['size']];
-        unset($options['size']);
-        
-        // Configura o estilo do botão...
-        if (!array_key_exists($options['style'], $this->styles)) {
-            $options['style'] = 'default';
-        }
-        $classes[] = $this->styles[$options['style']];
-        unset($options['style']);
         
         // Configura a URL do botão...
         $url = $options['url'];
         unset($options['url']);
         
-        // Configura a propriedade 'flat' do botão...
-        if (false !== (bool) $options['flat']) {
-            $classes[] = 'btn-flat';
-        }
-        unset($options['flat']);
-        
-        // Configura a propriedade 'block' do botão...
-        if (false !== (bool) $options['block']) {
-            $classes[] = 'btn-block';
-        }
-        unset($options['block']);
-        
         // Configura o ícone do botão...
-        $icon = '';
+        $iconHtml = '';
         if (null !== $options['icon']) {
-            $icon = $this->Icon->make($options['icon']);
+            $iconHtml = $this->Icon->make($options['icon']);
         }
         if (true === (bool) $options['iconLeft']) {
-            $text = sprintf('%s %s', $icon, $text);
+            $textHtml = $iconHtml . $textHtml;
         } else {
-            $text = sprintf('%s %s', $text, $icon);
+            $textHtml = $textHtml . $iconHtml;
         }
         unset($options['icon']);
         unset($options['iconLeft']);
-        
-        $options['class'] = trim(sprintf('%s %s', $options['class'], implode(' ', $classes)));
         
         // Configura a tag do botão...
         $tag = $options['tag'];
@@ -168,8 +82,11 @@ class ButtonHelper extends Helper
         
         // Constroi e retorna o botão, de acordo com a tag desejada
         switch ($tag) {
-            case 'link':   return $this->makeLink($text, $url, $options);
-            case 'button': return $this->makeButton($text, $options);
+            case 'a':
+            case 'link':
+                return $this->makeLink($textHtml, $url, $options);
+            case 'button':
+                return $this->makeButton($textHtml, $options);
         }
     }
     
