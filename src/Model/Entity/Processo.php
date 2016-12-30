bacfc3338fd8a4bdf8301c652f18f194929b5e58
Renato Uchoa <renatoubra@gmail.com>
2016-12-30 13:02:05 -0300

    Melhorias na entidade Processo
    - Implementação de métodos para retornar o período do processo;
    - Documentação dos métodos já implementados;
    
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

--- src/Model/Entity/Processo.php
+++ src/Model/Entity/Processo.php
@@ -6,6 +6,12 @@ use Cake\ORM\Entity;
 
 class Processo extends Entity
 {
+    /**
+     * Retorna todas as modalidades de ensino atendidas pelo processo
+     * concatenadas e separadas por vírgula
+     * 
+     * @return string
+     */
     protected function _getModalidades()
     {
         $modalidades = [];
@@ -15,6 +21,11 @@ class Processo extends Entity
         return implode(', ', $modalidades);
     }
     
+    /**
+     * Retorna o público total do processo
+     * 
+     * @return int
+     */
     protected function _getPublico()
     {
         $publico = 0;
@@ -22,5 +33,32 @@ class Processo extends Entity
             $publico += $processoModalidade->publico;
         }
         return $publico;
+    }
+    
+    /**
+     * Retorna a qualtidade de dias atendidos pelo processo
+     * 
+     * @return int
+     */
+    protected function _getPeriodo ()
+    {
+        $dias = [];
+        foreach ($this->cardapios as $cardapio) {
+            foreach ($cardapio->atendimentos as $atendimento) {
+                $dias[$atendimento->data->i18nFormat()] = true;
+            }
+        }
+        return count($dias);
+    }
+    
+    /**
+     * Retorna a quantidade de dias acompanhado de 'dia' ou 'dias'
+     * 
+     * @return string
+     */
+    protected function _getPeriodoText ()
+    {
+        $periodo = $this->periodo;
+        return sprintf('%d %s', $periodo, $periodo == 1 ? 'dia' : 'dias');
     }
 }
