b15018d3d6188775da3f65a1838686b84eb1be09
Renato Uchoa <renatoubra@gmail.com>
2017-01-04 15:46:20 -0300

    Implementação da funcionalidade "Cadastrar Cardápio"
    - resolve #81
    
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

--- src/Controller/CardapiosController.php
+++ src/Controller/CardapiosController.php
@@ -2,6 +2,9 @@
 
 namespace Enutri\Controller;
 
+use Cake\Datasource\Exception\RecordNotFoundException;
+use Enutri\Model\Entity\Uex;
+
 class CardapiosController extends AppController
 {
     /**
@@ -16,17 +19,68 @@ class CardapiosController extends AppController
     {
         if (!$this->usuarioLogado->lotado($uex)) {
             $this->Flash->error('Selecione uma UEx válida.');
-            return $this->redirect(['action' => 'selecionarUex']);
+            return $this->redirect([
+                'controller' => 'Processos',
+                'action' => 'selecionarUex',
+            ]);
         }
     }
     
+    /**
+     * Visualização das informações do cardápio especificado
+     * 
+     * @param type $cardapioId
+     * 
+     * @return void
+     */
     public function visualizar ($cardapioId = null)
     {
         try {
             $cardapio = $this->Cardapios->localizar($cardapioId);
+            $this->verificarPermissao($cardapio->processo->participante->uex);
             $this->set(compact('cardapio'));
-        } catch (Exception $ex) {
-
+        } catch (RecordNotFoundException $e) {
+            $this->Flash->error('Cardápio inválido.');
+            return $this->redirect([
+                'controller' => 'Processos',
+                'action' => 'selecionarUex',
+            ]);
+        }
+    }
+    
+    /**
+     * Cadastro de novo cardápio
+     * 
+     * @param int $processoId
+     * 
+     * @return void
+     */
+    public function cadastrar ($processoId = null)
+    {
+        try {
+            $this->loadModel('Processos');
+            $processo = $this->Processos->localizar($processoId);
+            $this->verificarPermissao($processo->participante->uex);
+            $cardapio = $this->Cardapios->newEntity();
+            if ($this->request->is(['post', 'put'])) {
+                $this->Cardapios->patchEntity($cardapio, $this->request->data);
+                $cardapio->processo_id = $processo->id;
+                if ($this->Cardapios->save($cardapio)) {
+                    $this->Flash->success('Cardápio cadastrado com sucesso.');
+                    return $this->redirect(['action' => 'visualizar', $cardapio->id]);
+                }
+                $this->Flash->error('Não foi possivel salvar o cardápio.');
+            }
+            $this->loadModel('CardapioTipos');
+            $this->set('cardapioTipos', $this->CardapioTipos->getList());
+            $this->set(compact('cardapio'));
+            $this->set(compact('processo'));
+        } catch (RecordNotFoundException $e) {
+            $this->Flash->error('Processo inválido.');
+            return $this->redirect([
+                'controller' => 'Processos',
+                'action' => 'selecionarUex',
+            ]);
         }
     }
 }
